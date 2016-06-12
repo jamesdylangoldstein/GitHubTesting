cd00aae1ae4c3e4f2beec0fe3d2d58ee71c649c3
James Goldstein <jamesdylangoldstein@gmail.com>
2016-06-12 10:29:28 -0700

    Initial Commit

---
+++ GitHubTesting/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GitHubTesting
+//
+//  Created by James Dylan Goldstein on 6/12/16.
+//  Copyright © 2016 James Dylan Goldstein. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
